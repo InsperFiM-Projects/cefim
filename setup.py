041ea80ee3d2f0a7d0692be79129a55023969880
Gustavo Amarante <32441206+gusamarante@users.noreply.github.com>
2025-03-19 18:07:34 -0300

    Update setup.py

--- setup.py
+++ setup.py
@@ -7,12 +7,12 @@ here = os.path.abspath(os.path.dirname(__file__))
 with codecs.open(os.path.join(here, "README.md"), encoding="utf-8") as fh:
     long_description = "\n" + fh.read()
 
-VERSION = '0.1'
+VERSION = '0.2'
 DESCRIPTION = "Repository of Insper's Finance and Macro Unit"
 
 # Setting up
 setup(
-    name="cefin",
+    name="cefim",
     version=VERSION,
     author="Insper's Finance Unit",
     maintainer="Insper's Finance Unit",
@@ -21,7 +21,7 @@ setup(
     long_description_content_type="text/markdown",
     long_description=long_description,
     packages=find_packages(),
-    install_requires=[  # TODO Update this
+    install_requires=[
         'pandas',
         'requests',
     ],
